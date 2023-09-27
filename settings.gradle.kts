fd5a75bafb177f7d3a662c40889651400e89aab3
maotou-spy <phucbuihuu2604@gmail.com>
2023-09-27 22:31:10 +0700

    project init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "prm392-gp1"
+include(":app")
