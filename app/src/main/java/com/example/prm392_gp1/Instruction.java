1b489c1a344526c21ef1cd60dc1819d9be8bb3b8
just-do-it <125887496+thanhnd29@users.noreply.github.com>
2023-10-02 21:52:44 +0700

    Add .idea/misc.xml file

--- app/src/main/java/com/example/prm392_gp1/Instruction.java
+++ app/src/main/java/com/example/prm392_gp1/Instruction.java
@@ -3,12 +3,19 @@ package com.example.prm392_gp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Instruction extends AppCompatActivity {
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruction);
+
+       MusicPlayer.playAudioFromResource(this, R.raw.waiting);
     }
+
+
 }
